8948edf50337549ffaaef94d46e79b1facf44260
PashaKim <kimrotwarrior@gmail.com>
2017-06-01 18:02:09 +0300

    Initial comment

---
+++ Leap-year.py
@@ -0,0 +1,7 @@
+print ("This function(is_year_leap(year)) specifies a leap year")
+def is_year_leap (y):
+    if y/4==y//4:
+        print(y," is leap year")
+    else:
+        print(y," is common year")
+        
