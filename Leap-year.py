499e739ec2e7a144eba7a3b4b3934f36d3286ac8
Psvel <kimrotwarrior@gmail.com>
2017-06-07 18:27:16 +0300

    Add files via upload

--- Leap-year.py
+++ Leap-year.py
@@ -1,6 +1,8 @@
 print ("This function(is_year_leap(year)) specifies a leap year")
 def is_year_leap (y):
-    if y/400==y//400:
+    if y%400==0:
+        print(y," is leap year")
+    elif y%4==0 and y%100!=0:
         print(y," is leap year")
     else:
         print(y," is common year")
